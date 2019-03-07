5b72237ac322f65db0d0ddf10676c7c0e3dd2aba
mrha140799 <mrha140799@gmail.com>
2019-03-07 20:53:30 +0700

    1st

---
+++ Circle.java
@@ -0,0 +1,38 @@
+package ComparableCircle;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle() {
+        this.radius = 1.0;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI*Math.pow(this.radius , 2);
+    }
+    public double getPerimeter() {
+        return 2*Math.PI*this.radius;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius= " + this.radius+", which is a subclass of " + super.toString();
+    }
+}
---
+++ ComparableCircle.java
@@ -0,0 +1,17 @@
+package ComparableCircle;
+
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle> {
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+}
